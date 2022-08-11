1cd99e637167e0654fdcc046a3bb759e95c0f203
peanutxh <91866923+peanutxh@users.noreply.github.com>
2022-08-11 16:37:14 +0000

    Add files via upload

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 
-// Button should link to {setSelected} to set {selected} to random number, thereby returning a quote in anecdotes[i]
-const Button = (props) => {
-  return (
-      <button onClick={props.onClick}> {props.text} </button>
-  )
-};
+const AnecVote = ({text, count}) => (
+  <div> 
+    <p> {text} </p> 
+    <p> upvotes: {count} </p> 
+  </div>
+);
+
+const Button = (props) => <button onClick={props.onClick}> {props.text} </button>;
 
 //rng but doesn't generate max 
-const rng = (max) => {
-  return Math.floor(Math.random() * max);
-};
+const rng = (max) => Math.floor(Math.random() * max);
 
 //returns index of highest value in an array
 const indexOfMax = (arr) => {
@@ -31,16 +31,6 @@ const indexOfMax = (arr) => {
   return maxIndex;
 };
 
-const AnecVote = ({text, count}) => {
-  return (
-    <> 
-      <div>
-        <p> {text} </p>
-        <p> upvotes: {count} </p>
-      </div>
-    </>
-  )
-};
 
 const App = () => {
   const anecdotes = [
@@ -56,7 +46,8 @@ const App = () => {
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState( Array(anecdotes.length).fill(0) );
   
-  //setState is asynchronous >> can't see immediate updates
+  // Button link to {setSelected} to set {selected} to random number, returning a quote in anecdotes[selected]
+  // setState is asynchronous >> can't see immediate updates
   const handleVoteClicks = () => {
     let newVote = [...votes][selected] + 1;
     setVotes(votes.fill(newVote, selected, selected+1));
@@ -74,8 +65,7 @@ const App = () => {
       <h2> Anecdote with the most upvotes </h2>
       <AnecVote text={anecdotes[indexOfMax(votes)]} count={Math.max(...votes)} />
     </div>
-  )
+  );
 };
 
 export default App;
-
